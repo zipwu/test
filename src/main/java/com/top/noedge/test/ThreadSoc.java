d2c61d0a01de56360c3676fd85b13163a3ae733c
wujiaye <blockcode@163.com>
2018-03-13 18:44:50 +0800

    add ThreadSoc

---
+++ src/main/java/com/top/noedge/test/ThreadSoc.java
@@ -0,0 +1,10 @@
+package com.top.noedge.test;
+
+public class ThreadSoc {
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World!" );
+	        System.err.println("Here is an error!");
+	    }
+
+}
